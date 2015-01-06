2ae3671544a7dac68a645712f3c4d8dcabef2214
Joshua Roberts Peterson <joshuapr1@gmail.com>
2015-01-05 23:12:26 -0600

    Update reverse_string.rb

--- reverse_string.rb
+++ reverse_string.rb
@@ -2,7 +2,7 @@ require 'rest_client'
 require 'json'
 
 res = RestClient.post "http://challenge.code2040.org/api/getstring", {
-  :email => 'joshuapr1@gmail.com',
+  :email => 'aaaaaaa@gmail.com', #this is a fake email
   :github => 'https://github.com/jpr71',
   :token => 'dNs4ea9kg1'
   }.to_json
@@ -10,7 +10,7 @@ res = RestClient.post "http://challenge.code2040.org/api/getstring", {
 hash = JSON.parse(res)
 
 res1 = RestClient.post "http://challenge.code2040.org/api/validatestring", {
-  :email => 'joshuapr1@gmail.com',
+  :email => 'aaaaaaa@gmail.com', #this is a fake email,
   :github => 'https://github.com/jpr71',
   :token => 'dNs4ea9kg1',
   :string => hash["result"].reverse!
